5315732844e95d9b1d3d6cca97606a2a6f3221a5
Dan Goyette <goyette@staff.gwi.net>
2009-06-04 13:56:26 -0400

    Latest Change

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -13,7 +13,7 @@ public partial class _Default : System.Web.UI.Page
     }
     protected void Button1_Click(object sender, EventArgs e)
     {
-        Label1.Text = "Testing";
+        Label1.Text = "Testing!!!";
         Label1.Text = DateTime.Now.ToLongTimeString();
 
     }
