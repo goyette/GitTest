8f341e58c8cbf310ee7cd43d2fb43640215a48e0
Dan Goyette <goyette@staff.gwi.net>
2009-06-04 13:47:34 -0400

    Changed to use Longer dates

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -13,6 +13,6 @@ public partial class _Default : System.Web.UI.Page
     }
     protected void Button1_Click(object sender, EventArgs e)
     {
-        Label1.Text = DateTime.Now.ToShortDateString();
+        Label1.Text = DateTime.Now.ToLongTimeString();
     }
 }
